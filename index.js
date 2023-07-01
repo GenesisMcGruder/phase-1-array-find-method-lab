a73fa627a24522411d26561176b3956b342f6575
Genesis McGruder <genesismcgruder@gmail.com>
2023-07-01 00:31:28 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin (record) {
+    let answer = record.find(element => element.result === "W")
+   if (answer == null) {
+     return undefined
+   } else {
+      return answer.year;
+   }
+ }
+superbowlWin(record)
